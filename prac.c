2008174c571d044dfb993214cc6f75596a4dc6d1
jazzmatic-jazz <88478403+jazzmatic-jazz@users.noreply.github.com>
2021-08-05 19:30:36 +0530

    Code Chef
    Mirror image question at code chef.

--- prac.c
+++ prac.c
@@ -1,68 +1,25 @@
 #include <stdio.h>
 
-int pa(int arr[], int size);
-int quicksort(int arr[], int low, int high);
-int part(int arr[], int low,int high);
-int swap(int *a, int *b);
-
 int main()
 {
-	int arr[]={5,7,1,6,10};
-	int size= sizeof (arr)/sizeof (arr[0]);
+	int i, n, t,r,sum=0;
+	scanf("%d",&t);
 	
-	pa(arr, size);
-	
-	quicksort(arr, 0, size-1);
-	printf("\n");
-	pa(arr, size);
-}
-
-int pa(int arr[], int size)
-{
-	int i;
-	
-	for (i=0; i<size; i++)
+	while(t>0)
 	{
-		printf("%d ", arr[i]);
-	}
-}
-
-int quicksort(int arr[], int low, int high)
-{
-	int pi;
-	if (low<high)
-	{
-		pi= part(arr, low, high);
+		scanf("%d",&n);
 		
-		quicksort(arr, low, pi-1);
-		
-		quicksort(arr, pi+1, high);
-	}
-}
-
-int part(int arr[], int low,int high)
-{
-	int v=arr[high];
-	int l=(low-1);
-	int j;
-	
-	for (j=low; j<=high-1; j++)
-	{
-		if (arr[j]<v)
+		while(n!=0)
 		{
-			l++;
-			swap(&arr[l],&arr[j]);
+			r=n%10;
+			sum=sum+r;
+			n=n/10;
 		}
 		
+		t--;
+		
+		printf("%d\n",sum);
+		sum=0;
 	}
-	swap(&arr[l+1],&arr[high]);
-	return(l+1);
-}
-
-int swap(int *a, int *b)
-{
-	int temp;
-	temp=*a;
-	*a=*b;
-	*b=temp;
+	
 }
